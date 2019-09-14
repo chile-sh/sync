67f4db8388e9ffcc293e8a6f5ca48eceb18d68a0
Mauricio Allende <mauricio.allende.o@gmail.com>
2019-09-13 23:56:29 -0300

    add `/create` route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,65 @@
 import fastify from 'fastify'
+import nanoid from 'nanoid'
+
 import config from './config'
 import logger from './logger'
+import redis from './redis'
+import { createHash, encrypt } from './helpers'
 
 const app = fastify({ logger })
 
-app.get('/', async () => {
-  return { success: true }
+app.route({
+  method: 'POST',
+  url: '/create',
+  schema: {
+    body: {
+      type: 'object',
+      required: ['content'],
+      properties: {
+        content: { type: ['string', 'number', 'object'] },
+        ttl: { type: 'number', maximum: config.maxTTL },
+        deleteAfterOpen: { type: 'number' },
+      },
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          viewHash: { type: 'string' },
+          adminHash: { type: 'string' },
+          expireDate: { type: 'number' },
+          viewed: { type: 'number' },
+        },
+      },
+    },
+  },
+
+  handler: async (request, reply) => {
+    const { content, ttl = 3600 } = request.body
+
+    const adminHash = createHash(nanoid()).slice(0, 32)
+    const viewHash = createHash(adminHash)
+
+    const str =
+      typeof content === 'object' ? JSON.stringify(content) : String(content)
+
+    if (str.length > config.maxSize) {
+      reply.code(413)
+      throw Error(`'content' cannot be larger than ${config.maxSize} bytes`)
+    }
+
+    const encodedStr = encrypt(str, adminHash)
+
+    const expireDate = Date.now() + ttl * 1000
+
+    await redis.set(viewHash, encodedStr, 'EX', ttl)
+
+    return {
+      viewHash,
+      adminHash,
+      expireDate,
+    }
+  },
 })
 
 const main = async () => {
